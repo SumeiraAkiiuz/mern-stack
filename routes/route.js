d8ec75cc3bc051389e11183465c2341c65dd1f8b
SumeiraAkkiuz <akyuz.sumeyragulsum@gmail.com>
2020-12-10 14:14:21 -0500

    authrouter added

--- routes/route.js
+++ routes/route.js
@@ -4,11 +4,6 @@ const router = express.Router();
 
 
 router.get("/", (req, res)=>{
-    console.log("Home page");
-    res.send("home page");
-})
-
-router.get("/api", (req, res)=>{
     console.log("api page");
     res.send("api page");
 })
