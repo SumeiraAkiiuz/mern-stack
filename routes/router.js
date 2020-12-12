cc7102dde73ebe9e70c1b04709e67b7b692eff9d
SumeiraAkkiuz <akyuz.sumeyragulsum@gmail.com>
2020-12-12 07:33:24 -0500

    profile, book and connect db files added

--- routes/router.js
+++ routes/router.js
@@ -2,10 +2,31 @@ const express = require("express");
 
 const router = express.Router();
 const AuthRouter = require("./AuthRouter");
+const ProfileRouter = require("./ProfileRouter")
+const BookRouter = require("./BookRouter")
 
-//only api endpoint
+
+//only /api endpoint
+
+/**
+ * @route /api/auth
+ * @description Route for auth
+ */
 
 router.use("/auth", AuthRouter);
 
+/**
+ * @route /api/profile
+ * @description Route for profile
+ */
+
+ router.use("/profile", ProfileRouter);
+
+ /**
+ * @route /api/book
+ * @description Route for book
+ */
+
+ router.use("/book", BookRouter)
 
 module.exports= router;
