c5d9ad41a7911591ffc589b37bff84f757b7389b
SumeiraAkkiuz <akyuz.sumeyragulsum@gmail.com>
2020-12-11 20:03:33 -0500

    * turned into /

--- routes/router.js
+++ routes/router.js
@@ -2,8 +2,12 @@ const express = require("express");
 
 const router = express.Router();
 
-
 router.get("/", (req, res)=>{
+    console.log("Home Page");
+    res.send("Home Page")
+})
+
+router.get("/api", (req, res)=>{
     console.log("api page");
     res.send("api page");
 })
--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require("express");
 
 const app = express();
 
-const router = require("./routes/route");
+const router = require("./routes/router");
 
-app.use("*", router);
+app.use("/", router);
+
+
 
 app.listen(3000, ()=>{
     console.log("i am listening on port 3000");
